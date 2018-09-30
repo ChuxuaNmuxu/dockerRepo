67f4eb9fab132a3c29d60978f16a0b89b869377e
sunb001 <2267660581@qq.com>
2018-09-30 11:20:23 +0800

    test image auto build

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 http.createServer(function(req, res){
   res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end('welcom to league')
+  res.end('welcom to league !!!!')
 }).listen(8082)
 
 console.log('server running')
